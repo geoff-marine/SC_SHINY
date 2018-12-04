e8cdb28809dc0a17e7d978f0bb71cbdefd5a0ae2
geoff-marine <45462029+geoff-marine@users.noreply.github.com>
2018-12-04 16:22:34 +0000

    change in GITHUB - different line to RStudio

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
-   # Application title
+   # Application title - change in line 15
    titlePanel("changed titlepanel in GITHUB"),
    
    # Sidebar with a slider input for number of bins 
